76da3d8038246114377a3452ecb25fc502713162
Maksim GAYDUK <mgayduk@e2r11p11.unit.ua>
2017-12-04 11:01:12 +0200

    Fixed algo bug and memory leaks

--- src/transformer.c
+++ src/transformer.c
@@ -6,7 +6,7 @@
 /*   By: mgayduk <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/01 17:25:11 by mgayduk           #+#    #+#             */
-/*   Updated: 2017/12/02 13:37:46 by mgayduk          ###   ########.fr       */
+/*   Updated: 2017/12/04 10:56:52 by mgayduk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -135,6 +135,5 @@ t_tetro				*fetch_coords(t_list *head, int tet_num)
 		count++;
 	}
 	free_composition(composition);
-	show_tetro_list(coord_list);
 	return (coord_list);
 }
